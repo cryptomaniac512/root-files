ec8b93f60596b400fe210a00990f8d7661cc9b38
Nikita CryptoManiac Sivakov <cryptomaniac.512@gmail.com>
2018-03-28 14:06:37 +1000

    A bit fixes for polybar and i3wm

--- home/cryptomaniac/.config/i3/exec_always.sh
+++ home/cryptomaniac/.config/i3/exec_always.sh
@@ -2,8 +2,7 @@
 
 ### polybar
 killall -q polybar
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null;do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar top &
 
 ### nm-applet
