81ba9f0dbed81643c3a3be7165e42cb479642bc0
Nikita Sivakov <sivakov512@gmail.com>
2021-02-05 22:43:34 +0300

    Temporary disable xss-lock

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -59,7 +59,7 @@
       defaultSession = "none+i3";
       sessionCommands = ''
         test -r $HOME/.Xresources && ${pkgs.xorg.xrdb}/bin/xrdb -merge $HOME/.Xresources
-        ${pkgs.xss-lock}/bin/xss-lock -- ${pkgs.lightdm}/bin/dm-tool lock &
+        ## ${pkgs.xss-lock}/bin/xss-lock -- ${pkgs.lightdm}/bin/dm-tool lock &
       '';
 
       lightdm = {
